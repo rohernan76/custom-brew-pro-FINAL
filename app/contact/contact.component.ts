e354811821b1deb51d6ad46f5c516a6db737cd75
rohernan76 <rohernan@gmail.com>
2016-09-01 16:10:27 -0600

    added closing tags to input

--- app/contact/contact.component.ts
+++ app/contact/contact.component.ts
@@ -1,5 +1,4 @@
 import { Component } from '@angular/core';
-import { ContactSubmit } from './contact.submit';
 
 @Component({
 	selector: 'contact',
@@ -9,16 +8,16 @@ import { ContactSubmit } from './contact.submit';
 			<h3>Quick Contact</h3>
 			<h4>Contact me today, and get a reply within 24 hours!</h4>
 		<fieldset>
-			<input placeholder="Your name" type="text" tabindex="1" required autofocus>
+			<input placeholder="Your name" type="text" tabindex="1" required autofocus/>
 		</fieldset>
 		<fieldset>
-			<input placeholder="Your Email Address" type="email" tabindex="2" required>
+			<input placeholder="Your Email Address" type="email" tabindex="2" required/>
 		</fieldset>
 		<fieldset>
-			<input placeholder="Your Phone Number" type="tel" tabindex="3" required>
+			<input placeholder="Your Phone Number" type="tel" tabindex="3" required/>
 		</fieldset>
 		<fieldset>
-			<input placeholder="Your Web Site starts with http://" type="url" tabindex="4" required>
+			<input placeholder="Your Web Site starts with http://" type="url" tabindex="4" required/>
 		</fieldset>
 		<fieldset>
 			<textarea placeholder="Type your Message Here...." tabindex="5" required></textarea>
