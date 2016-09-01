7a77f0c6544fe0d0efa14fd2053385f62f74b57f
rohernan76 <rohernan@gmail.com>
2016-09-01 15:35:48 -0600

    Fix capitalization in beer service

--- app/beer.service.ts
+++ app/beer.service.ts
@@ -1,6 +1,6 @@
 import { Injectable } from "@angular/core";
 import { ApiService } from "./api.service";
-import { Observable } from "rxjs/observable";
+import { Observable } from "rxjs/Observable";
 
 @Injectable()
 export class BeerService {
